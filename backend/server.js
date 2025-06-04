77ffe01a95190015d4c35ee2647219bd1ff81e9e
Sakin <sakin@Sakins-MacBook-Air.local>
2025-06-04 23:34:19 +0600

    Initial commit with frontend and backend

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import connectDB from './config/mongodb.js'; // Add `.js` if using ES modules
+import connectCloudinary from './config/cloudinary.js';
+import adminRouter from './routes/doctorRoute.js';
+// Connect to MongoDB
+connectDB()
+connectCloudinary()
+
+// App config
+const app = express();
+const port = process.env.PORT || 4000;
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+//api endpoint
+app.use('/api/admin',adminRouter)
+//localhost:4000/api/admin
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('API working great ✅');
+});
+
+// Start server
+app.listen(port, () => {
+  console.log(`🚀 Server running on http://localhost:${port}`);
+});
